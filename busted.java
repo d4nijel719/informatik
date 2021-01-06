cd886454f4a5f69bd79d18f399498f5744fb01d9
Poolitzer213 <detering.lars@gmail.com>
2021-01-06 09:37:30 -0800

    added busted

---
+++ busted.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+ 
+       public static void main (String[] args)
+       {
+             // Ausgabe Hrld!
+		int a  1;
+	int b =
+	int c = a+b;
+             System.oln("Hello World!");
+       }
+}
