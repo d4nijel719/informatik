2675739748957c4de47160fa2398371290f9eddf
Poolitzer213 <detering.lars@gmail.com>
2021-01-06 09:30:53 -0800

    added integer

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
        public static void main (String[] args)
        {
              // Ausgabe Hello World!
+		int a = 1;
+	int b = 2;
+	int c = a+b;
              System.out.println("Hello World!");
        }
 }
